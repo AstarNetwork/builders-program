6b17e474caf3904fe9c5389392f2143bfaea2b6e
Maarten <36227274+fiexer@users.noreply.github.com>
2021-10-14 09:11:41 +0200

    Create hellowold.sol

---
+++ hellowold.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.22;
+
+/* 
+dApp staking whitelisted contract
+
+Project overview:
+<INFORMATION>
+
+We are part of the builders program, check out our proposal here:
+<FORUM LINK>
+
+You can find us here:
+Telegram: 
+Discord: 
+Twitter: 
+
+
+*/
+
+contract helloWorld {
+ function renderHelloWorld () public pure returns (string) {
+   return 'helloWorld';
+ }
+}
